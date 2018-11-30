874adcbe531c9b8244aed80ef03d34c8cea02e75
xiansili <865915497@qq.com>
2018-11-30 19:13:41 +0800

    购物车计算属性

--- 计算属性/购物车（计算属性）/shop/index.js
+++ 计算属性/购物车（计算属性）/shop/index.js
@@ -1,56 +1,63 @@
 var app = new Vue({
-    el: "#app",
-    data: {
-      list: [
-        {
-          id:1,
-          name: "iphone7",
-          price: 6199,
-          count: 1
-        },
-        {
-          id:2,
-          name: "ipad",
-          price: 2299,
-          count: 1
-        },
-        {
-          id:3,
-          name: "apple",
-          price: 355,
-          count: 1
-        },
-        {
-          id:4,
-          name: "banana",
-          price: 298,
-          count: 1
-        }
-      ],
-    },
+  el: "#app",
+  data: {
+    list: [
+      {
+        id: 1,
+        name: "iphone7",
+        price: 6199,
+        count: 1,
+        isshow:false
+      },
+      {
+        id: 2,
+        name: "ipad",
+        price: 2299,
+        count: 1,
+        isshow:false
+      },
+      {
+        id: 3,
+        name: "apple",
+        price: 355,
+        count: 1,
+        isshow:false
+      },
+      {
+        id: 4,
+        name: "banana",
+        price: 298,
+        count: 1,
+        isshow:false
+      }
+    ]
+  },
 
-    methods:{
-        less: function(index){
-            if (this.list[index].count ===1) return;
-               
-             this.list[index].count --; 
-        },
-        add: function(index){
-            this.list[index].count ++;
-        },
-        remove: function(index){
-             this.list.splice(index, 1)
-        }
+  methods: {
+    less: function(index) {
+      if (this.list[index].count === 1) return;
 
+      this.list[index].count--;
     },
-    computed:{
-        shopprice:function () {
-            var prices = 0;
-            for (var i = 0; i < this.list.length; i++) {
-                var item = this.list[i];
-                prices += item.price * item.count;
-            }
-            return prices.toString().replace(/\B(?=(\d{3})+$)/g,',');
-        }
+    add: function(index) {
+      this.list[index].count++;
+    },
+    remove: function(index) {
+      this.list.splice(index, 1);
+    },select: function(index) {
+      this.list[index].isshow = ! this.list[index].isshow;
+     },
+  },
+  computed: {
+    shop(){
+      let sum = 0;
+      this.list.forEach(elem=>{
+        if (elem.isshow) {
+          let money =  elem.price * elem.count;
+           sum += money;
+        }   
+      });
+      return sum.toString().replace(/\B(?=(\d{3})+$)/g, ",");;
     }
-  });
+  }
+});
